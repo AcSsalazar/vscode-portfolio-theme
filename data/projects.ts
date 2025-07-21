2cc3b2a59c43b54d3b5df5eb33de3aa2f6139038
AcSsalazar <acsalazar@unal.edu.co>
2025-07-21 16:23:33 -0500

    Logos and personal projects update

--- data/projects.ts
+++ data/projects.ts
@@ -8,35 +8,35 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: 'Driwwwle',
+    title: 'Market Place with Wompi',
     description:
-      'Discover creative websites and developers. A portal for you to share your projects.',
-    logo: '/logos/driwwwle.svg',
-    link: 'https://github.com/itsnitinr/driwwwle',
+      'Fully functional marketplace web application integrated with Bancolombia’s Wompi payment gateway.',
+    logo: '/logos/mp-icon1.svg',
+    link: 'https://github.com/AcSsalazar/Market_Place_with_Wompi_API',
     slug: 'driwwwle',
   },
   {
-    title: 'VSCode Portfolio',
+    title: 'VSCode Portfolio Fork',
     description:
-      'A Visual Studio Code themed developer portfolio built with Next.js and CSS Modules.',
+      'A Visual Studio Code–themed developer portfolio built with Next.js and CSS Modules, including skills visualizations.',
     logo: '/logos/vsc.svg',
     link: 'https://github.com/itsnitinr/vscode-portfolio',
     slug: 'vscode-portfolio',
   },
   {
-    title: 'Subtrackt',
+    title: 'XGB_Diabetes_Classificator',
     description:
-      'A simple and elegant way to track your subscriptions and save money.',
-    logo: '/logos/subtrackt.svg',
-    link: 'https://github.com/itsnitinr/subtrackt',
+      'A complete predictor for diabetes diagnosis using pre-trained XGBoost and Random Forest models. Includes a user interface and input preprocessing.',
+    logo: '/logos/ML-icon.svg',
+    link: 'https://github.com/AcSsalazar/XGB_Diabetes_Classificator',
     slug: 'subtrackt',
   },
   {
-    title: 'Coolify Deployments',
+    title: 'RAG system with Pinecone and LangChain',
     description:
-      'VSCode extension to track and deploy your Coolify applications.',
-    logo: '/logos/coolify.svg',
-    link: 'https://github.com/itsnitinr/coolify-vscode-extension',
+      'Retrieval-Augmented Generation (RAG) system using Pinecone vector database and LangChain for document-based question answering.',
+    logo: '/logos/ai-platform.svg',
+    link: 'https://github.com/AcSsalazar/RAG_with_LangChain_and_Pinecone',
     slug: 'coolify-vscode-extension',
   },
 ];
